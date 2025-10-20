babedb91bdb39bb2f7d3c4a5db01328828fe63d5
GauravRaj997 <crk0732@gmail.com>
2025-10-20 13:44:04 +0530

    Initial commit: short clear message

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = 3000;
+
+// Serve static files from "public" folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// Routes
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "home.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "about.html"));
+});
+
+app.get("/contact", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "contact.html"));
+});
+
+// Start Server
+app.listen(3000, () => {
+  console.log("Server running at http://localhost:3000");
+});
